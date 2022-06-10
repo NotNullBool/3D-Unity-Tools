955de8a629e11fa49dcdc18e35924184c85d75da
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2022-06-10 06:36:14 -0400

    Formating

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,15 +1,39 @@
+#region Copyright (c)
+/****************************************
+ * Copyright (c) All rights reserved to
+ * !NullBool, NotNullBool
+ * Contact info: notnullbool@gmail.com
+ ****************************************/ 
+#endregion
+
+#region Imports
+using System.Collections;
+using System.Collections.Generic;
+using static LanguageExt.Prelude;
 using UnityEngine;
+#endregion
+
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Variables
+
+    #endregion
+    
+    #region Methods
+    /// <summary>
+    /// <see cref="Start"/> is called before the first frame update
+    /// </summary>
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// <see cref="Update"/> is called once per frame
+    /// </summary>
     void Update()
     {
         
     }
+    #endregion Methods
 }
