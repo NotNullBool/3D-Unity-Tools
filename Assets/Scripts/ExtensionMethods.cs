a1a24601d348052575d84188f9f4e0a07d0a0358
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2022-06-10 16:44:02 -0400

    OutToVar added to remember variables calculated in conditionals (hopefully very useful in the future!)

--- Assets/Scripts/ExtensionMethods.cs
+++ Assets/Scripts/ExtensionMethods.cs
@@ -11,6 +11,7 @@ using System.Collections;
 using System.Collections.Generic;
 using static LanguageExt.Prelude;
 using UnityEngine;
+using System.Diagnostics.Contracts;
 #endregion
 
 namespace ExtensionMethods
@@ -22,8 +23,26 @@ namespace ExtensionMethods
         #endregion
         
         #region Methods
+        /// <summary>
+        /// Get component from current object if not add it
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        [Pure]
         public static T GetAddComponent<T>(this GameObject gameObject) where T : Component => 
-        gameObject.GetComponent<T>() != null ? gameObject.GetComponent<T>() : gameObject.AddComponent<T>();
+        gameObject.GetComponent<T>().OutToVar(out var component) != null ? component : gameObject.AddComponent<T>();
+
+        /// <summary>
+        /// Set this to a new variable.
+        /// Useful to save variable from conditionals.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="obj"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        [Pure]
+        public static T OutToVar<T>(this T @this,out T obj) => obj = @this;
         #endregion Methods
     }
 }
