6c52ad2feaf5ca103d9ccd48d7870ab2ae5dc9ea
NotNullBull <dptjvoy9@anonredirect.anonaddy.com>
2022-06-15 11:08:42 -0400

    Input setup

--- Assets/CustomPlayerInput.cs
+++ Assets/CustomPlayerInput.cs
@@ -10,34 +10,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using static LanguageExt.Prelude;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine;
-using ExtensionMethods;
 #endregion
 
-public class Movement : MonoBehaviour
+public class CustomPlayerInput : MonoBehaviour
 {
     #region Variables
-
-    [SerializeField]
-    private Rigidbody rigidBody;
-    [SerializeField]
-    private CharacterController characterController;
-
+       public InputActionAsset inputActionAsset;
     #endregion
     
     #region Methods
-
-    /// <summary>
-    /// <see cref="Awake"/> is called before Start
-    /// </summary>
-    void Awake()
-    {
-        rigidBody = gameObject.IfNullGetAddComponent(rigidBody);
-        rigidBody.freezeRotation = true;
-        
-        characterController =  gameObject.IfNullGetAddComponent(characterController);
-    }
-
     /// <summary>
     /// <see cref="Start"/> is called before the first frame update
     /// </summary>
@@ -51,10 +35,7 @@ public class Movement : MonoBehaviour
     /// </summary>
     void Update()
     {
-        float speed = 1f * Time.deltaTime ;
-        characterController.Move(transform.forward * speed);
+        
     }
-
-    void Test(Vector2 position) => Debug.Log(position);
     #endregion Methods
 }
